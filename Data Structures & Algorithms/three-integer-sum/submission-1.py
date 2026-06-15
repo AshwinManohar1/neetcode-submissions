062eb1372b3a3f737dfddf68959de5fed3ca682a
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-06-15 09:31:36 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        # 3 pointers. but array not sorted , and contains negative numbers. 
+
+        n = len(nums)
+        if n<3:
+            return []
+        output = []
+        nums.sort()
+
+        left = 0 
+        right = n -2
+
+        for i in range(n-2):
+            if i > 0 and nums[i] ==nums[i-1]:
+                continue
+            left, right = i+1, n-1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    output.append([nums[i], nums[left], nums[right]])
+                    left +=1
+                    right -=1
+
+                    while left< right and nums[left] == nums[left-1]:
+                        left+=1
+                    while left <right and nums[right] == nums[right+1]:
+                        right-=1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -=1
+
+            
+        return output
+
+
+        
