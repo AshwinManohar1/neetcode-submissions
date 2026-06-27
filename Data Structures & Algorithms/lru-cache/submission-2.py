685d5341ef48105f20ebb356a6cf7474f78a3222
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-06-27 17:54:23 +0530

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self ,key = 0, val = 0):
+        self.key = key
+        self.val = val 
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+        self.cache = {}
+
+    def remove(self , node):
+        p, n = node.prev , node.next
+        p.next = n
+        n.prev = p
+
+    def _insert_at_top(self , node):
+        real_pointer = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = real_pointer
+        real_pointer.prev = node
+
+
+    def get(self, key: int) -> int:
+        
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+
+        self.remove(node)
+        self._insert_at_top(node)
+
+        return node.val
+
+        
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            node = self.cache[key]
+
+            node.val = value
+
+            self.remove(node)
+            self._insert_at_top(node)
+
+        else:
+            new_node = Node(key , value)
+            self.cache[key]= new_node
+            self._insert_at_top(new_node)
+
+
+
+        if len(self.cache) > self.capacity:
+            lru_cache = self.tail.prev
+            del self.cache[lru_cache.key]
+            self.remove(lru_cache)
+
+
+
+        
+
+
+        
