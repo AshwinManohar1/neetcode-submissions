88a2592c6859144bdd238841f4da57d306104d81
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-07-05 11:33:46 +0530

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+
+        queue = deque([root])
+
+        while queue:
+            e= queue.popleft()
+            if e.val == subRoot.val:
+                if self.isSameTree(e , subRoot):
+                    return True
+
+            if e.left is not None:
+                queue.append(e.left)
+            if e.right is not None:
+                queue.append(e.right)
+
+        return False
+
+    def isSameTree(self , p , q):
+
+        # if not p and not q:
+        #     return True
+        
+        # if not p or not q:
+        #     return False
+        
+        # if p.val != q.val:
+        #     return False
+
+        # return self.isSameTree(p.left , q.left) and self.isSameTree(p.right , q.right)
+
+        queue = deque([(p , q)])
+
+        while queue:
+            node1 , node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            queue.append((node1.left , node2.left))
+            queue.append((node1.right , node2.right))
+
+        return True
+            
+                
+        
