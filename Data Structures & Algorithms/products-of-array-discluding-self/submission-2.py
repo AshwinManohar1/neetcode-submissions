80c2a80f978f6a165e6a7b9a2db0071c3753b693
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-07-12 11:45:09 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,71 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+            #ill get the nums , i want to calculate the sum leaving the number
+            # output = []
+            # for index , number in enumerate(nums):
+            #     curr_sum = 1
+            #     for i in range(len(nums)):
+            #         if index == i:
+            #             continue;
+                    
+            #         curr_sum *= nums[i]
+
+            #     output.append(curr_sum)
+
+            # return output
+
+            # n = len(nums)
+            # output = [1] * n
+
+            # prefix = 1
+            # for i in range(n):
+            #     output[i] = prefix
+            #     prefix *= nums[i]
+
+            # postfix = 1
+            # for i in range(n-1 , -1 , -1):
+            #     output[i] *= postfix
+            #     postfix *= nums[i]
+
+            # return output 
+
+
+
+
+
+        # prefix , postfix
+
+        n = len(nums)
+
+        output = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            output[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(n-1 , -1 , -1):
+            output[i] *= postfix
+            postfix *= nums[i]
+
+        return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
