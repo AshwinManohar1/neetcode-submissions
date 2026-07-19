4a3b0ebdaaa45e998384f0a8e0c70e76e9e66d7d
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-07-19 15:39:44 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+
+        subset = []
+
+        def dfs(current_index):
+
+            if current_index >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[current_index])
+            dfs(current_index+1)
+
+            subset.pop()
+            dfs(current_index+1)
+
+        dfs(0)
+        return res
