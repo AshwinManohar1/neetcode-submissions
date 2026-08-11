e0a5afe18f2b606dc3926032b8f5f964642f9614
AshwinManohar1 <114246024+AshwinManohar1@users.noreply.github.com>
2026-08-11 21:50:52 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,55 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # if not grid:
+        #     return 0
+
+        # ROWS , COLS = len(grid) , len(grid[0])
+        # visited = set()
+        # islands = 0
+
+        # def bfs(r , c):
+        #     q = collections.deque()
+        #     visited.add((r, c))
+        #     q.append((r,c))
+
+        #     while q:
+        #         row, col = q.popleft()
+        #         directions = [[1, 0],[-1 , 0],[0 , 1], [0 , -1]]
+        #         for dr , dc in directions:
+        #             r , c = row+dr , col+dc
+
+        #             if ((r in range(ROWS)) and (c in range(COLS)) and grid[r][c] == '1' and (r , c) not in visited):
+        #                 q.append((r, c))
+        #                 visited.add((r, c))
+
+
+
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if grid[r][c] == '1' and (r, c) not in visited:
+        #             bfs(r , c)
+        #             islands+=1
+        
+        # return islands
+
+        directions = [[1, 0],[-1, 0], [0,1],[0,-1]]
+        ROWS, COLS =len(grid), len(grid[0])
+
+        islands = 0
+
+        def dfs(r, c):
+            if(r<0 or c<0 or r>=ROWS or c >=COLS or grid[r][c] == "0"):
+                return 
+
+            grid[r][c] = "0"
+            for dr , dc in directions:
+                dfs(r+dr, c+dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands+= 1
+
+        return islands
+
